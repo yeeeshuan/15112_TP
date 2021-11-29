5c47e12f6b5365ab781c18dedc4b78833c5041be
yeeeshuan <yehuang2020>
2021-11-28 21:18:00 -0500

    Added GameModes

--- main.py
+++ main.py
@@ -154,6 +154,7 @@ def appStarted(app):
 # mousemoved
 # tracks if mouse is moved to position of help bottom or play button
 def mouseMoved(app, event):
+    # tracks which gamemode user chooses
     if distance(event.x, event.y, app.width // 4, 3 * app.height // 4) <= 60:
         app.gameStart = False
         app.gameMode_1 = True
@@ -165,18 +166,15 @@ def mouseMoved(app, event):
     app.startX = event.x
     app.startY = event.y
 
-
-# tracks the brightest pixel on the screen as tracker
-# combined tacking code from https://www.pyimagesearch.com/2014/09/29/
-# in collaboration with Alina Chen (alinache)
-# finding-brightest-spot-image-using-python-opencv/ with 112 graphics
+#cameraFired (openCV + 112Graphics)
 def cameraFired(app):
-    # tracks the index finger on screen as tracker
-    # tracking code from
-    # https://www.analyticsvidhya.com/blog/2021/07/building-a-hand-tracking-system-using-opencv/
     app.combo = False
     app.critical_hit = False
 
+    # tracks the brightest pixel on the screen as tracker
+    # combined tacking code from https://www.pyimagesearch.com/2014/09/29/
+    # in collaboration with Alina Chen (alinache)
+    # finding-brightest-spot-image-using-python-opencv/ with 112 graphics
     if app.gameMode_1:
         app.frame = cv2.flip(app.frame, 1)
         # converts image to grayscale
@@ -188,8 +186,11 @@ def cameraFired(app):
         app.y = maxLoc[1]
         cv2.circle(app.frame, maxLoc, 5, (255, 0, 0), 2)
 
-        cv2.imshow("Tracker", app.frame)
+        cv2.imshow("Tracker_1", app.frame)
 
+    # tracks the index finger on screen as tracker
+    # tracking code from
+    # https://www.analyticsvidhya.com/blog/2021/07/building-a-hand-tracking-system-using-opencv/
     if app.gameMode_2:
         img = cv2.flip(app.frame, 1)
         mpHands = mp.solutions.hands
@@ -197,15 +198,12 @@ def cameraFired(app):
                           max_num_hands=2,
                           min_detection_confidence=0.5,
                           min_tracking_confidence=0.5)
-        mpDraw = mp.solutions.drawing_utils
 
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results = hands.process(imgRGB)
-        # print(results.multi_hand_landmarks)
         if results.multi_hand_landmarks:
             for handLms in results.multi_hand_landmarks:
                 for id, lm in enumerate(handLms.landmark):
-                    # print(id,lm)
                     h, w, c = img.shape
                     cx, cy = int(lm.x * w), int(lm.y * h)
                     cv2.circle(img, (cx, cy), 3, (255, 0, 255), cv2.FILLED)
@@ -213,7 +211,7 @@ def cameraFired(app):
                         app.x = cx
                         app.y = cy
 
-        cv2.imshow("Image", img)
+        cv2.imshow("Tracker_2", img)
 
     # if fruit is sliced, set coordinates for falling and split to True
     # if fruit is sliced, set coordinates for falling and split to True
